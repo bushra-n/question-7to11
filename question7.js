29b24a2bd9ac95084f4c9d5dbe1f4a5db27a1596
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-13 17:30:52 +0500

    Add files via upload

---
+++ question7.js
@@ -0,0 +1,18 @@
+// task no.10 adding comment
+// athuor [Bushra ALI]
+// Date [19, Friday, 2024]
+// Task no.7 & 8
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+// task no.9
+var favouriteNumber = 5;
+// revealing my favourite number in a message formate.
+console.log("My favourite number is ".concat(favouriteNumber, "."));
+//  task no.11
+var names = ["Bushra", "Ammar", "Rabia", "Tehmina"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
---
+++ question7.ts
@@ -0,0 +1,21 @@
+// task no.10 adding comment
+// athuor [Bushra ALI]
+// Date [19, Friday, 2024]
+
+
+// Task no.7 & 8
+console.log(5+3)
+console.log(10-2)
+console.log(4*2)
+console.log(16/2)
+
+// task no.9
+let favouriteNumber:number= 5;
+// revealing my favourite number in a message formate.
+console.log(`My favourite number is ${favouriteNumber}.`);
+//  task no.11
+let names:string [] = ["Bushra","Ammar","Rabia","Tehmina"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
